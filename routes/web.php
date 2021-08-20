bc2fb7dfcfdedc4840069e1cb0d5872e9e2edbf9
Sheva Ardhana <59791072+shevaardhana@users.noreply.github.com>
2021-08-20 12:56:13 +0700

    fix login, edit dasboard, & logout

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [App\Http\Controllers\DashBoardController::class, 'index'])->middleware('auth')->name('DashBoard');
+
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
